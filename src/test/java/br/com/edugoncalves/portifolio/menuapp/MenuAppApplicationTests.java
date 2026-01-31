6be971ab12c356dfef8074f27cb6fae269f31566
Eduardo Gonçalves <edupersonalprojects@users.noreply.github.com>
2026-01-30 23:44:34 -0300

    :construction: building - first commit

---
+++ src/test/java/br/com/edugoncalves/portifolio/menuapp/MenuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edugoncalves.portifolio.menuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
